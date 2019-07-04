d463aed6ae64baf79884c0caad00e75509bbdfd0
chao <22640673+chao7150@users.noreply.github.com>
2019-07-04 09:42:49 +0900

    add tests of formatMs

--- src/components/atoms/__tests__/Clock.test.tsx
+++ src/components/atoms/__tests__/Clock.test.tsx
@@ -4,6 +4,17 @@ test("100000ms must be written as 01:40", () => {
   expect(formatMs(100000)).toBe("01:40");
 });
 
-test("less than 1000ms must be treated as 1sec", () => {
+test("60000ms must be written as 01:00", () => {
+  expect(formatMs(60000)).toBe("01:00");
+});
+
+test("6000000ms must be written as 100:00", () => {
+  expect(formatMs(6000000)).toBe("100:00");
+});
+
+test("[1000 * n - 999, 1000 * n] must be treated as n sec", () => {
+  expect(formatMs(0)).toBe("00:00");
+  expect(formatMs(1)).toBe("00:01");
   expect(formatMs(999)).toBe("00:01");
+  expect(formatMs(1000)).toBe("00:01");
 });
